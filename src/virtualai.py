040db4a5a5f30dd96605a50858109a3d9ef7de2f
borbelytomii <55625187+borbelytomii@users.noreply.github.com>
2022-11-23 13:27:12 +0100

    src: Refactor

--- src/virtualai.py
+++ src/virtualai.py
@@ -3,7 +3,6 @@ import requests
 import datetime
 import sys
 import os
-from winsound import PlaySound
 from gtts import gTTS
 from playsound import playsound
 from pprint import pprint
@@ -11,275 +10,162 @@ from os.path import dirname, abspath
 d = dirname((dirname(abspath(__file__))))
 sys.path.append(d)
 import config as api
-# API_KEY='' in config.py
 api.API_KEY
-
-
+err="Say that again sir"
 def take_commands():
-
     # Making the use of Recognizer and Microphone
-    # Method from Speech Recognition for taking
-    # commands
     r = sr.Recognizer()
-
     with sr.Microphone() as source:
         print('Listening')
-
-        # seconds of non-speaking audio before
-        # a phrase is considered complete
         r.pause_threshold = 0.7
         audio = r.listen(source)
         try:
             print("Recognizing")
-
             # for listening the command in  english
-            Query = r.recognize_google(audio, language='en-US')
-
+            query = r.recognize_google(audio, language='en-US')
             # for printing the query or the command that we give
-            print("The Inline query is printed'", Query, "'")
+            print("The Inline query is printed'", query, "'")
         except Exception as e:
-
-            # this method is for handling the exception
-            # and so that assistant can ask for telling
-            # again the command
             print(e)
-            Speak("Say that again sir")
+            speak(err)
             return "None"
-
-    return Query
-
-# Speak engine
-
-
-def Speak(text):
+    return query
+# speak engine
+def speak(text):
     tts = gTTS(text=text, lang='en')
     filename = 'sounds/voice.mp3'
     tts.save(filename)
     playsound(filename)
     os.remove(filename)
-
-# Startup sound
-
-
-def Startup():
+# startup sound
+def startup():
     startup = 'sounds/startup.mp3'
     playsound(startup)
-
-
-def tellDay():
-
-    # the weekday method is a method from datetime
-    # library which helps us to an integer
-    # corresponding to the day of the week
-    # this dictionary will help us to map the
-    # integer with the day and we will check for
-    # the condition and if the condition is true
-    # it will return the day
+def tell_day():
     day = datetime.datetime.today().weekday() + 1
-
-    Day_dict = {1: 'Monday', 2: 'Tuesday', 3: 'Wednesday',
+    day_dict = {1: 'Monday', 2: 'Tuesday', 3: 'Wednesday',
                 4: 'Thursday', 5: 'Friday', 6: 'Saturday',
                 7: 'Sunday'}
-
-    if day in Day_dict.keys():
-        day_of_the_week = Day_dict[day]
+    if day in day_dict.keys():
+        day_of_the_week = day_dict[day]
         print(day_of_the_week)
-        Speak("The day is " + day_of_the_week)
-
-
-def Worktime():
-
+        speak("The day is " + day_of_the_week)
+def work_time():
     day = datetime.datetime.today().weekday() + 1
-
-    Day_dict = {1: 'Monday', 2: 'Tuesday', 3: 'Wednesday',
+    day_dict = {1: 'Monday', 2: 'Tuesday', 3: 'Wednesday',
                 4: 'Thursday', 5: 'Friday', 6: 'Saturday',
                 7: 'Sunday'}
-
-    if day in Day_dict.keys():
-        day_of_the_week = Day_dict[day]
+    if day in day_dict.keys():
+        day_of_the_week = day_dict[day]
         print(day_of_the_week)
-        #todays_day = day_of_the_week
-        #Speak("The day is " + day_of_the_week)
-
         if day_of_the_week == 'Monday':
-            Speak('Your weekend is over. Go to work.')
+            speak('Your weekend is over. Go to work.')
         elif day_of_the_week == 'Sunday' or day_of_the_week == 'Saturday':
-            Speak('Today is off.')
+            speak('Today is off.')
         else:
-            Speak('Go to work.')
-
-
-def tellTime():
-
+            speak('Go to work.')
+def tell_time():
     hour = str(datetime.datetime.today().hour)
     minutes = str(datetime.datetime.today().minute)
-
+    timestamp="It's: "+hour+": "+minutes
+    greetings=""
     time = datetime.datetime.now().hour
     # print(time)
-
     day_time = {1: 'Morning', 2: 'Noon',
                 3: 'Afternoon', 4: 'Evening', 5: 'Night'}
-
     if time < 12 and time >= 5:
         print(day_time[1])
-        Speak("Good" + day_time[1]+"Sir")
-        print("It's: "+hour+": "+minutes)
-        Speak("It's: "+hour+": "+minutes)
-
+        greetings="Good" + day_time[1]+"Sir"
     if time == 12:
         print(day_time[2])
-        Speak("Good" + day_time[2]+"Sir")
-        print("It's: "+hour+": "+minutes)
-        Speak("It's: "+hour+": "+minutes)
-
+        greetings="Good" + day_time[2]+"Sir"
     if time > 12 and time <= 17:
         print(day_time[3])
-        Speak("Good" + day_time[3]+"Sir")
-        print("It's: "+hour+": "+minutes)
-        Speak("It's: "+hour+": "+minutes)
-
+        greetings="Good" + day_time[3]+"Sir"
     if time > 17 and time <= 21:
         print(day_time[4])
-        Speak("Good" + day_time[4]+"Sir")
-        print("It's: "+hour+": "+minutes)
-        Speak("It's: "+hour+": "+minutes)
-
+        greetings="Good" + day_time[4]+"Sir"
     if time > 21 and time <= 4:
         print(day_time[5])
-        Speak("Good" + day_time[5]+"Sir")
-        print("It's: "+hour+": "+minutes)
-        Speak("It's: "+hour+": "+minutes)
-
-
-def tellCity():
+        greetings="Good" + day_time[5]+"Sir"
+    speak(greetings)
+    print(timestamp)
+    speak(timestamp)
+def tell_city():
     # City name
     r = sr.Recognizer()
-
     with sr.Microphone() as source:
         print('Listening')
-
         # seconds of non-speaking audio before
         # a phrase is considered complete
         r.pause_threshold = 0.7
         audio = r.listen(source)
         try:
             print("Recognizing City name")
-
             city_name = r.recognize_google(audio, language='en-US')
-
             # for printing the name of the city
             print("The name of the City'", city_name, "'")
         except Exception as e:
-
             # this method is for handling the exception
-            # and so that assistant can ask for telling
+            # and so that assistant can ask for tell_ing
             # again the command
             print(e)
-            Speak("Say that again sir")
+            speak(err)
             return "None"
-
     return city_name
-
-
-def tellWeather():
-    #city = input('Enter your city : ')
-    city = tellCity()
-    # Url address with api key
-    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api.API_KEY}&units=metric'
-
+def tell_weather(speak):
+    if speak:
+        city = tell_city()
+    else:
+        city= "Veszprem"
+       # Url address with api key
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api.API_KEY}&units=metric'
     res = requests.get(url)
-
     data = res.json()
-
     temp = data['main']['temp']
     wind_speed = data['wind']['speed']
-
     latitude = data['coord']['lat']
     longitude = data['coord']['lon']
     description = data['weather'][0]['description']
-
     # Callouts
-    Speak('Temperature : {} degree celcius'.format(temp))
+    speak('Temperature : {} degree celcius'.format(temp))
     print('Temperature : {} degree celcius'.format(temp))
     print('Wind Speed : {} m/s'.format(wind_speed))
     print('Latitude : {}'.format(latitude))
     print('Longitude : {}'.format(longitude))
-    Speak('Description : {}'.format(description))
+    speak('Description : {}'.format(description))
     print('Description : {}'.format(description))
-
-
-def defaultWeather():
-    city = "Veszprem"
-    # Url address with api key
-    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api.API_KEY}&units=metric'
-
-    res = requests.get(url)
-
-    data = res.json()
-
-    temp = data['main']['temp']
-    wind_speed = data['wind']['speed']
-
-    latitude = data['coord']['lat']
-    longitude = data['coord']['lon']
-    description = data['weather'][0]['description']
-
-    # Callouts
-    Speak('Temperature : {} degree celcius'.format(temp))
-    print('Temperature : {} degree celcius'.format(temp))
-    print('Wind Speed : {} m/s'.format(wind_speed))
-    print('Latitude : {}'.format(latitude))
-    print('Longitude : {}'.format(longitude))
-    Speak('Description : {}'.format(description))
-    print('Description : {}'.format(description))
-
-
-def tellCommands():
+def tell_commands():
     # City name
     r = sr.Recognizer()
-
     with sr.Microphone() as source:
         print('Listening')
-
-        # seconds of non-speaking audio before
-        # a phrase is considered complete
         r.pause_threshold = 0.7
         audio = r.listen(source)
         try:
             print("Recognizing.. ")
-
             voice_command = r.recognize_google(audio, language='en-US')
-
             # for printing the name of the city
             print("The command'", voice_command, "'")
         except Exception as e:
-
-            # this method is for handling the exception
-            # and so that assistant can ask for telling
-            # again the command
             print(e)
-            Speak("Say that again sir")
+            speak(err)
             return "None"
-
     return voice_command
-
-
 # Driver Code
 if __name__ == '__main__':
-    Startup()
-    Speak("How Can I help you today Sir?")
-    while tellCommands() != 'over':
+    startup()
+    speak("How Can I help you today Sir?")
+    while tell_commands() != 'over':
         command = take_commands()
         if "morning" in command:
-            tellDay()
-            tellTime()
-            defaultWeather()
-            Worktime()
-
+            tell_day()
+            tell_time()
+            tell_weather(False)
+            work_time()
         if "day" in command:
-            tellDay()
-            tellTime()
-
+            tell_day()
+            tell_time()
         if "weather" in command:
-            # tellCity()
-            tellWeather()
+            # tell_City()
+            tell_weather(True)
